bce112b0098a1aef36f090a0d916efaf0a8b0063
Afonso <47607813+Feijix@users.noreply.github.com>
2022-05-19 15:04:20 +0100

    some changes

--- event-app/components/events/event-item.js
+++ event-app/components/events/event-item.js
@@ -1,4 +1,46 @@
+import Button from "../ui/button";
+import classes from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+
 export default function EventItem(props) {
-  return <li>
-  </li>;
+  const { event } = props;
+
+  const readableDate = new Date(event.date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = event.location.replace(", ", "\n");
+
+  const exploreLink = `/events/${event.id}`;
+
+  return (
+    <li key={event.id} className={classes.item}>
+      <img src={event.image} alt={event.description} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{event.title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{readableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
 }
--- event-app/pages/events/[event_id].js
+++ event-app/pages/events/[event_id].js
@@ -1,11 +1,15 @@
 import { useRouter } from "next/router";
+import { getEventById } from "../../data/dummy-data";
 
 export default function Event() {
   const router = useRouter();
+  const event = getEventById(router.query.event_id);
+
+  if (!event) return <p>No event found!</p>;
 
   return (
     <>
-      <h1>{`Event ${router.query.event_id}`}</h1>
+      <h1>{`Event ${event.title}`}</h1>
     </>
   );
 }
