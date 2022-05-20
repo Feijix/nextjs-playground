b1b3fafb92a634e25ec05ab4ebba4cc0d20e3330
Afonso <47607813+Feijix@users.noreply.github.com>
2022-05-20 22:11:41 +0100

    after section 5

---
+++ render-fetch-miniproj/pages/last-sales.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+export default function LastSalesPages(props) {
+  const [sales, setSales] = useState(props.sales);
+
+  const { data, error } = useSWR(process.env.DB_URL + "sales.json", (url) =>
+    fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  if (error) return <p>Failed to load.</p>;
+  if (!data || !sales) return <p>Loading...</p>;
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  return fetch(process.env.DB_URL + "sales.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      return { props: { sales: transformedSales }, revalidate: 10 };
+    });
+}
